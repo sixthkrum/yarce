75d8964300b7d20cc85b99036a18cf9e6686e92a
sixthkrum <sixthkrum@gmail.com>
2024-12-31 16:32:45 +0530

    remove stackprof

--- Gemfile
+++ Gemfile
@@ -1,4 +1,3 @@
 source "https://rubygems.org" do
   gem "ruby2d", "~> 0.12.1"
-  gem 'stackprof'
 end
--- src/main.rb
+++ src/main.rb
@@ -49,109 +49,107 @@ sleep_times = []
 
 window_manager = YARCE::WindowManagers::Alpha.new({ title: 'Chip8' })
 
-StackProf.run(mode: :cpu, out: 'stackprof-output.dump') do
-  Thread.abort_on_exception = true
+Thread.abort_on_exception = true
 
-  # making another thread for input handling to make the blocking aspect reside in the device implementation and not
-  # the implementation that calls the device
-  # TODO: look into making all the concurrent stuff into fibers
-  Thread.new do
-    while true do
-      result = window_manager.window_directive_handler.read
+# making another thread for input handling to make the blocking aspect reside in the device implementation and not
+# the implementation that calls the device
+# TODO: look into making all the concurrent stuff into fibers
+Thread.new do
+  while true do
+    result = window_manager.window_directive_handler.read
 
-      if result
-        input_data = window_manager.window_directive_handler.data
+    if result
+      input_data = window_manager.window_directive_handler.data
 
-        case input_data[0]
-        when 1 # key down code
-          if input_data[1] < 0x10
-            device.key_pressed_this_frame = input_data[1]
-            device.keypad_state[device.key_pressed_this_frame] = true
-          else
-            case input_data[1]
-            when 0x10 # space
-              # open debugging menu
-            when 0x11 # escape
-              # exit
-              exit(0)
-            else
-              nil
-            end
-          end
-        when 3 # key up code
-          device.keypad_state[input_data[1]] = false
+      case input_data[0]
+      when 1 # key down code
+        if input_data[1] < 0x10
+          device.key_pressed_this_frame = input_data[1]
+          device.keypad_state[device.key_pressed_this_frame] = true
         else
-          nil
+          case input_data[1]
+          when 0x10 # space
+            # open debugging menu
+          when 0x11 # escape
+            # exit
+            exit(0)
+          else
+            nil
+          end
         end
+      when 3 # key up code
+        device.keypad_state[input_data[1]] = false
+      else
+        nil
       end
-
-      sleep seconds_per_instruction / 2
     end
+
+    sleep seconds_per_instruction / 2
   end
+end
 
-  # thread for decrementing timers and playing sound
-  tone = Ruby2D::Music.new('./assets/tone-512.wav')
-  tone.loop = true
-  tone_currently_playing = false
-  Thread.new do
-    last_time = Time.now.to_f
-
-    while true do
-      sleep 1.0 / 60.0
-
-      device.decrement_delay_timer
-      device.decrement_sound_timer
-
-      if tone_currently_playing && device.sound_timer == 0
-        tone.stop
-        tone_currently_playing = false
-      elsif !tone_currently_playing && device.sound_timer > 0
-        tone.play
-        tone_currently_playing = true
-      end
-
-      decrement_time = Time.now.to_f
-      decrement_times << (decrement_time - last_time)
-
-      last_time = decrement_time
-    end
-  end
+# thread for decrementing timers and playing sound
+tone = Ruby2D::Music.new('./assets/tone-512.wav')
+tone.loop = true
+tone_currently_playing = false
+Thread.new do
+  last_time = Time.now.to_f
 
   while true do
-    instruction_start_time = Time.now.to_f
+    sleep 1.0 / 60.0
 
-    current_high_byte = device.memory[device.program_counter]
-    current_low_byte = device.memory[device.program_counter + 1]
+    device.decrement_delay_timer
+    device.decrement_sound_timer
 
-    break if current_high_byte.nil? || current_low_byte.nil?
-
-    input_nibbles = [current_high_byte / 16, current_high_byte & 0b00001111,
-                     current_low_byte / 16, current_low_byte & 0b00001111]
-
-    device.execute_instruction(*input_nibbles)
-
-    if device.last_instruction == :drw_vx_vy_n
-      window_manager.window_directive_handler.write(device.display)
+    if tone_currently_playing && device.sound_timer == 0
+      tone.stop
+      tone_currently_playing = false
+    elsif !tone_currently_playing && device.sound_timer > 0
+      tone.play
+      tone_currently_playing = true
     end
 
-    # program counter being nil handles the return case wherein the stack is empty
-    break if (device.program_counter.nil? || device.program_counter > 0xFFF)
+    decrement_time = Time.now.to_f
+    decrement_times << (decrement_time - last_time)
 
-    instruction_end_time = Time.now.to_f
-    instruction_time = (instruction_end_time - instruction_start_time)
-    instruction_times << instruction_time
+    last_time = decrement_time
+  end
+end
 
-    sleep_time = (seconds_per_instruction - instruction_time)
-    sleep_times << sleep_time
-    sleep_time = 0 if sleep_time < 0
+while true do
+  instruction_start_time = Time.now.to_f
 
-    # reset the key pressed this frame to prevent buffered input scenarios
-    device.key_pressed_this_frame = nil
+  current_high_byte = device.memory[device.program_counter]
+  current_low_byte = device.memory[device.program_counter + 1]
 
-    sleep sleep_time
+  break if current_high_byte.nil? || current_low_byte.nil?
+
+  input_nibbles = [current_high_byte / 16, current_high_byte & 0b00001111,
+                   current_low_byte / 16, current_low_byte & 0b00001111]
+
+  device.execute_instruction(*input_nibbles)
+
+  if device.last_instruction == :drw_vx_vy_n
+    window_manager.window_directive_handler.write(device.display)
   end
 
-  while true do
-    sleep seconds_per_instruction
-  end
+  # program counter being nil handles the return case wherein the stack is empty
+  break if (device.program_counter.nil? || device.program_counter > 0xFFF)
+
+  instruction_end_time = Time.now.to_f
+  instruction_time = (instruction_end_time - instruction_start_time)
+  instruction_times << instruction_time
+
+  sleep_time = (seconds_per_instruction - instruction_time)
+  sleep_times << sleep_time
+  sleep_time = 0 if sleep_time < 0
+
+  # reset the key pressed this frame to prevent buffered input scenarios
+  device.key_pressed_this_frame = nil
+
+  sleep sleep_time
+end
+
+while true do
+  sleep seconds_per_instruction
 end
